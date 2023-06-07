578326b6bcd2b433e7eacd3b3fb5b679ce387934
Teej <96551689+teejcoder@users.noreply.github.com>
2023-06-07 16:45:47 +1000

    Latest Changes

--- server/controllers/api.js
+++ server/controllers/api.js
@@ -14,8 +14,67 @@ const configuration = new Configuration({
 
 const plaidClient = new PlaidApi(configuration);
 
+
 module.exports = {
-  createLinkToken: async (request, response) => {
+  
+  handler = Plaid.create({
+    clientName: "Plaid Quickstart",
+    // Optional, specify an array of ISO-3166-1 alpha-2 country
+    // codes to initialize Link; European countries will have GDPR
+    // consent panel
+    countryCodes: ["US"],
+    env: "sandbox",
+    // Replace with your public_key from the Dashboard
+    key: "067daf1cd400ac6ef1e15d35f6edb5",
+    product: ["transactions"],
+    // Optional, use webhooks to get transaction and error updates
+    webhook: "https://requestb.in",
+    // Optional, specify a language to localize Link
+    language: "en",
+    // Optional, specify userLegalName and userEmailAddress to
+    // enable all Auth features
+    userLegalName: "John Appleseed",
+    userEmailAddress: "jappleseed@yourapp.com",
+    onLoad: function() {
+      // Optional, called when Link loads
+    },
+    onSuccess: function(public_token, metadata) {
+      // Send the public_token to your app server.
+      // The metadata object contains info about the institution the
+      // user selected and the account ID or IDs, if the
+      // Select Account view is enabled.
+      $.post("/plaid_token_exchange", {
+        publicToken: public_token
+      });
+    },
+    onExit: function(err, metadata) {
+      // The user exited the Link flow.
+      if (err != null) {
+        // The user encountered a Plaid API error prior to exiting.
+      }
+      // metadata contains information about the institution
+      // that the user selected and the most recent API request IDs.
+      // Storing this information can be helpful for support.
+    },
+    onEvent: function(eventName, metadata) {
+      // Optionally capture Link flow events, streamed through
+      // this callback as your users connect an Item to Plaid.
+      // For example:
+      // eventName = "TRANSITION_VIEW"
+      // metadata  = {
+      //   link_session_id: "123-abc",
+      //   mfa_type:        "questions",
+      //   timestamp:       "2017-09-14T14:42:19.350Z",
+      //   view_name:       "MFA",
+      // }
+    }
+  });
+
+
+
+
+
+  linkTokenCreateRequest: async (request, response) => {
     //   Get the client_user_id by searching for the current user);
     const clientUserId = request.body.id;
     console.log(clientUserId);
@@ -24,7 +83,7 @@ module.exports = {
         client_user_id: clientUserId,
       },
       client_name: "SpendWise",
-      products: ["transactions"],
+      products: ["transactions, auth, balance"],
       language: "en",
       redirect_uri: "/localhost:3000",
       country_codes: ["US"],
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const apiController = require("../controllers/api");
 
-router.post("/create_link_token", apiController.createLinkToken);
-router.post("/exchange_public_token", apiController.exchangePublicToken);
+router.post("/link/token/create", apiController.linkTokenCreateRequest);
+router.post("/item/public_token/exchange", apiController.exchangePublicToken);
 router.post("/transactions", apiController.transactions);
 router.delete("/account/:id", apiController.deleteAccount);
 router.get("/accounts/:id", apiController.getAccounts);
