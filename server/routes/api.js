27f8e4c7621b45079219f4f4f140b6e3b32fc535
Teej <96551689+teejcoder@users.noreply.github.com>
2023-06-09 00:45:31 +1000

    Latest Changes

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,8 @@
 const router = require("express").Router();
 const apiController = require("../controllers/api");
 
-router.post("/link/token/create", apiController.linkTokenCreateRequest);
-router.post("/item/public_token/exchange", apiController.exchangePublicToken);
+router.post("/api/create_link_token", apiController.createLink);
+router.post("/api/set_access_token", apiController.setAccess);
 router.post("/transactions", apiController.transactions);
 router.delete("/account/:id", apiController.deleteAccount);
 router.get("/accounts/:id", apiController.getAccounts);
